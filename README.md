0c46817e4e6f4950fdcb14f890acb6be775ab2c7
Leonid Lezner <kontakt@lezner.de>
2022-03-11 11:04:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sources
+
+https://blog.smittytone.net/2021/02/02/program-raspberry-pi-pico-c-mac/
---
+++ setup.sh
@@ -0,0 +1,5 @@
+brew install cmake
+brew tap ArmMbed/homebrew-formulae
+brew install arm-none-eabi-gcc
+
+git clone -b master --recurse-submodules https://github.com/raspberrypi/pico-sdk.git
